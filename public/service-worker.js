a27fa18807d18e56e16f71209b6d9aeeaa796396
Pavel Mogilevskyi <pavel13595@gmail.com>
2025-06-07 23:28:40 +0300

    1

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,14 +1,14 @@
-self.addEventListener('install', event => {
+self.addEventListener("install", (event) => {
   self.skipWaiting();
 });
 
-self.addEventListener('activate', event => {
+self.addEventListener("activate", (event) => {
   event.waitUntil(self.clients.claim());
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => {
+    caches.match(event.request).then((response) => {
       return response || fetch(event.request);
     })
   );
