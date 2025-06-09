9cc6df5c561a566d4e3f85575d8146897440084e
Pavel Mogilevskyi <pavel13595@gmail.com>
2025-06-09 19:33:04 +0300

    1

--- vite.config.ts
+++ vite.config.ts
@@ -8,24 +8,6 @@ export default defineConfig({
   plugins: [react(), vitePluginInjectDataLocator()],
   server: {
     allowedHosts: true,
-    // SPA fallback middleware
-    middlewareMode: false,
-    setupMiddlewares(middlewares, devServer) {
-      middlewares.use((req, res, next) => {
-        if (
-          req.url &&
-          !req.url.startsWith("/src") &&
-          !req.url.startsWith("/node_modules") &&
-          !req.url.startsWith("/@vite") &&
-          !req.url.startsWith("/public") &&
-          !req.url.includes(".") // нет расширения файла
-        ) {
-          req.url = "/index.html";
-        }
-        next();
-      });
-      return middlewares;
-    },
   },
   // Для production build (если используется netlify/vercel/static), тоже нужен fallback
   build: {
